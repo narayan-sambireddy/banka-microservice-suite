1eb950757ba123e3ff7407ec8e8e908076839766
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-08-12 00:36:11 +0530

    Added authorisation-server

---
+++ authoristion-server/src/main/java/narayan/banka/authoristionserver/AuthoristionServerApplication.java
@@ -0,0 +1,12 @@
+package narayan.banka.authoristionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthoristionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthoristionServerApplication.class, args);
+	}
+}
