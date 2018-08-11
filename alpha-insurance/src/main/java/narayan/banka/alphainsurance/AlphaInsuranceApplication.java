449fdb1d469cbe81b5a7cc72eacd98f918bb152e
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-08-12 00:35:20 +0530

    Added alpha-insurance

---
+++ alpha-insurance/src/main/java/narayan/banka/alphainsurance/AlphaInsuranceApplication.java
@@ -0,0 +1,12 @@
+package narayan.banka.alphainsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaInsuranceApplication.class, args);
+	}
+}
